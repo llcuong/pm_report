fae6b7c9b953722359c77455bf565a77ffde0818
ha-vu <onedvu315itech@gmail.com>
2025-11-06 10:05:36 +0700

    add config for tailwindcss

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,17 @@
+export default {
+  theme: {
+    extend: {
+      keyframes: {
+        loader: {
+          '0%': { left: '-40%', width: '40%' },
+          '50%': { left: '20%', width: '60%' },
+          '100%': { left: '100%', width: '40%' },
+        },
+      },
+      animation: {
+        loader: 'loader 1.2s ease-in-out infinite',
+      },
+    },
+  },
+  plugins: [],
+};
