4ab40b87169e1c2b2c951905487fec1cd099ffca
ha-vu <onedvu315itech@gmail.com>
2025-11-06 16:57:04 +0700

    add menu icon for pinhole report

---
+++ frontend/src/assets/icons/menu-icon.jsx
@@ -0,0 +1,12 @@
+const MenuIcon = () => {
+  return (
+    <svg className="w-6 h-6" viewBox="0 0 28 28" xmlns="http://www.w3.org/2000/svg" role="img">
+      <title>Menu</title>
+      <rect x="3" y="5" width="22" height="2.5" rx="1" fill="#000000" />
+      <rect x="3" y="12.75" width="22" height="2.5" rx="1" fill="#000000" />
+      <rect x="3" y="20.5" width="22" height="2.5" rx="1" fill="#000000" />
+    </svg>
+  );
+};
+
+export default MenuIcon;
