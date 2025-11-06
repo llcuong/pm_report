233529bd53db7867958690a7a76479763f139b98
ha-vu <onedvu315itech@gmail.com>
2025-11-06 10:24:54 +0700

    add unused import rules and use react hook linting

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,9 +1,11 @@
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import react from 'eslint-plugin-react'
-import prettier from 'eslint-plugin-prettier'
-import jsxA11y from 'eslint-plugin-jsx-a11y'
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import react from 'eslint-plugin-react';
+import prettier from 'eslint-plugin-prettier';
+import jsxA11y from 'eslint-plugin-jsx-a11y';
+import unusedImports from 'eslint-plugin-unused-imports';
+
 export default [
   {
     ignores: ['dist'],
@@ -19,18 +21,22 @@ export default [
     },
     plugins: {
       react,
-      prettier,
+      "no-unused-imports": unusedImports,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
       'jsx-a11y': jsxA11y,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
+      "react-hooks/rules-of-hooks": "error",
+      "react-hooks/exhaustive-deps": "warn",
       'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
       'react/jsx-uses-react': 'off',
       'react/react-in-jsx-scope': 'off',
       'jsx-a11y/alt-text': 'warn',
       'jsx-a11y/anchor-is-valid': 'warn',
+      "no-unused-vars": ["warn", { args: "none", ignoreRestSiblings: true }],
+      "no-unused-imports/no-unused-imports": "warn",
     },
     settings: {
       react: { version: 'detect' },
