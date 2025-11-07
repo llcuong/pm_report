b66cbf55f4e9745578aafd38457d5cda638b36e7
ha-vu <onedvu315itech@gmail.com>
2025-11-07 08:05:33 +0700

    add export default

--- frontend/src/components/DropdownDataCustom.jsx
+++ frontend/src/components/DropdownDataCustom.jsx
@@ -103,12 +103,12 @@ const DropdownDataCustom = ({
                 {option.badge && (
                   <span
                     className={`inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium ${{
-                        green: "bg-green-100 text-green-800",
-                        blue: "bg-blue-100 text-blue-800",
-                        red: "bg-red-100 text-red-800",
-                        orange: "bg-orange-100 text-orange-800",
-                        gray: "bg-gray-100 text-gray-800",
-                      }[option.badgeColor || "gray"]
+                      green: "bg-green-100 text-green-800",
+                      blue: "bg-blue-100 text-blue-800",
+                      red: "bg-red-100 text-red-800",
+                      orange: "bg-orange-100 text-orange-800",
+                      gray: "bg-gray-100 text-gray-800",
+                    }[option.badgeColor || "gray"]
                       }`}
                   >
                     {option.badge}
@@ -122,3 +122,5 @@ const DropdownDataCustom = ({
     </div>
   );
 };
+
+export default DropdownDataCustom;
