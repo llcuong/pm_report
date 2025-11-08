313b11637fd87a22e9b9322b0ca03d5a284279cc
ha-vu <onedvu315itech@gmail.com>
2025-11-08 09:48:16 +0700

    add login icon

--- frontend/src/assets/icons/login-icon.jsx
+++ frontend/src/assets/icons/login-icon.jsx
@@ -1,4 +1,4 @@
-const LoginIcon = ({ color = '#000000' }) => {
+const LoginIcon = () => {
   return (
     <svg className="w-6 h-6" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
       <title>Login</title>
