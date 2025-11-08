b72ba02c0617cf89b1b813184bec5da3f5f9f36a
ha-vu <onedvu315itech@gmail.com>
2025-11-08 09:55:24 +0700

    set default route when access app

--- frontend/src/routes/MainRoutes.jsx
+++ frontend/src/routes/MainRoutes.jsx
@@ -1,16 +1,22 @@
 import Loadable from "@components/Loadable";
 import MainLayout from "@layouts/MainLayout";
 import { lazy } from "react";
+import { Navigate } from "react-router-dom";
 
-const MainReport = Loadable(lazy(() => import('@modules/report/MainPinholeReport')));
+const MainPinholeReport = Loadable(lazy(() => import('@modules/pinhole/MainPinholeReport')));
 
 const MainRoutes = {
   path: '/',
   element: <MainLayout />,
   children: [
     {
+      // Set default route
+      index: true,
+      element: <Navigate to='pinhole-report' replace />
+    },
+    {
       path: 'pinhole-report',
-      element: <MainReport />
+      element: <MainPinholeReport />
     },
   ],
 };
