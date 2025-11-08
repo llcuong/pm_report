6407f5923cb1142989c8ca83e26293cddc2bb6aa
ha-vu <onedvu315itech@gmail.com>
2025-11-08 09:53:35 +0700

    switch Navbar and Sidebar

--- frontend/src/layouts/MainLayout.jsx
+++ frontend/src/layouts/MainLayout.jsx
@@ -4,11 +4,11 @@ import { Outlet } from "react-router-dom";
 
 export default function DashboardLayout() {
   return (
-    <div className="flex flex-col h-screen">
-      <Navbar />
-      <div className="flex flex-1 overflow-hidden">
-        <Sidebar />
-        <main className="flex-1 overflow-y-auto bg-gray-50 p-6">
+    <div className="flex h-screen">
+      <Sidebar />
+      <div className="flex flex-col w-screen">
+        <Navbar />
+        <main className="relative overflow-y-auto bg-gray-50 p-2">
           <Outlet />
         </main>
       </div>
