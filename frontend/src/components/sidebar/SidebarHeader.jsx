a24f4f33deb375bfdfc5eb8413997a05695e7a6f
ha-vu <onedvu315itech@gmail.com>
2025-11-08 09:52:09 +0700

    add menu icon

--- frontend/src/components/sidebar/SidebarHeader.jsx
+++ frontend/src/components/sidebar/SidebarHeader.jsx
@@ -1,10 +1,20 @@
-import React from "react"
+import MenuIcon from "@assets/icons/menu-icon";
 
-export default function SidebarHeader() {
-    return (
-        <div className="h-12 w-full flex flex-col items-center justify-center">
-            <div className="h-10"/>
-            <div className="w-10 h-[2px] bg-gray-200 rounded"/>
-        </div>
-    )
-}
+export default function SidebarHeader({ isExpanded }) {
+  return (
+    <div className="h-12 w-full flex flex-col items-center justify-center">
+      {
+        isExpanded ? (
+          <>
+            <span className="p-2 text-xl font-medium">Menu</span>
+          </>
+        ) : (
+          <div className="p-2">
+            <MenuIcon />
+          </div>
+        )
+      }
+      <div className={['h-0.5 bg-gray-400 rounded transition-all ease-linear', isExpanded ? 'w-52' : 'w-10'].join(' ')} />
+    </div>
+  );
+};
