c3130ca6f56395ad7411844d97226ec3e521da94
ha-vu <onedvu315itech@gmail.com>
2025-11-10 14:06:44 +0700

    set default header with token for everytime requests send

--- frontend/src/configs/api.js
+++ frontend/src/configs/api.js
@@ -1,33 +1,27 @@
+import getCookie from "@utils/getCookie";
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: 'http://172.18.55.215:17500',
+  baseURL: '/',                 // Un-direct to backend server, only direct to Vite through proxy configuration in vite.config.js
   headers: {
     'Content-Type': 'application/json',
   },
-  withCredentials: false,
+  withCredentials: true,
 });
 
-// const getCookie = (name) => {
-//   const value = `; ${document.cookie}`;
-//   const parts = value.split(`; ${name}=`);
-//   if (parts.length === 2)
-//     return decodeURIComponent(parts.pop().split(";").shift());
-//   return null;
-// };
+// Auto get CSRF Token every requests
+api.interceptors.request.use(
+  config => {
+    let token = getCookie('csrftoken');
+    if (token) {
+      config.headers['X-CSRFToken'] = token;
+    };
 
-// api.interceptors.request.use(
-//   config => {
-//     let token = getCookie('csrftoken');
-//     if (token) {
-//       config.headers['csrftoken'] = token;
-//     };
-
-//     return config;
-//   },
-//   error => {
-//     return Promise.reject(error);
-//   }
-// );
+    return config;
+  },
+  error => {
+    return Promise.reject(error);
+  }
+);
 
 export default api;
