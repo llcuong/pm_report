a30a25d354578fad385390ecdc57cc82ced643ad
ha-vu <onedvu315itech@gmail.com>
2025-11-10 14:06:02 +0700

    change route and icon name to sign in

--- frontend/src/components/sidebar/SidebarFooter.jsx
+++ frontend/src/components/sidebar/SidebarFooter.jsx
@@ -1,4 +1,4 @@
-import LoginIcon from "@assets/icons/login-icon";
+import SignInIcon from "@assets/icons/sign-in-icon";
 
 export default function SidebarFooter({ isExpanded }) {
   return (
@@ -6,17 +6,17 @@ export default function SidebarFooter({ isExpanded }) {
       <div className={['h-0.5 bg-gray-400 rounded transition-all ease-linear mb-1', isExpanded ? 'w-52' : 'w-10'].join(' ')} />
       <div className="flex items-center w-full h-full px-2 rounded-lg">
         <a
-          href='/login'
+          href='/sign-in'
           className="flex items-center w-full h-full rounded-lg text-center 
                      transition-colors duration-200 hover:bg-[#87c3c3] hover:text-white"
         >
           <div className="flex justify-center items-center w-10 h-10 text-xl">
-            <LoginIcon />
+            <SignInIcon />
           </div>
 
           {isExpanded && (
             <span className="ml-2 font-medium truncate">
-              Log In
+              Sign In
             </span>
           )}
         </a>
