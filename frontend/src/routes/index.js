077fae010c0072bf99c8279a63e5c60affba402d
ha-vu <onedvu315itech@gmail.com>
2025-11-10 14:09:15 +0700

    add authentication routes and admin routes

--- frontend/src/routes/index.js
+++ frontend/src/routes/index.js
@@ -1,8 +1,12 @@
 import { createBrowserRouter } from "react-router-dom";
 import MainRoutes from "./MainRoutes";
+import AuthRoutes from "./AuthRoutes";
+import AdminRoutes from "./AdminRoutes";
 
 const router = createBrowserRouter([
   MainRoutes,
+  AuthRoutes,
+  AdminRoutes,
 ], { basename: '/' });
 
 export default router;
