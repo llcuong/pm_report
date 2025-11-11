8ab7efbbe3ae355366b8314681fa72d0ad9935ef
ha-vu <onedvu315itech@gmail.com>
2025-11-11 14:42:38 +0700

    add new form icon

---
+++ frontend/src/assets/icons/circular-back-arrow-icon.jsx
@@ -0,0 +1,16 @@
+const CircularBackArrowIcon = () => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg"
+      width="24" height="24" viewBox="0 0 24 24"
+      role="img" aria-label="Go back"
+      fill="none" stroke="currentColor" strokeWidth="2"
+      strokeLinecap="round" strokeLinejoin="round">
+      <circle cx="12" cy="12" r="10"></circle>
+      <path d="M12 8 L8 12 L12 16"></path>
+      <path d="M16 12 H8"></path>
+    </svg>
+
+  );
+};
+
+export default CircularBackArrowIcon;
