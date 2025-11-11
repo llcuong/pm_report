b1215175ef01a54b7992145525bf0a975a0eb898
ha-vu <onedvu315itech@gmail.com>
2025-11-11 14:43:10 +0700

    use main layout for admin panel

--- frontend/src/routes/AdminRoutes.jsx
+++ frontend/src/routes/AdminRoutes.jsx
@@ -1,18 +1,19 @@
 import Loadable from "@components/Loadable";
 import ProtectedRoute from "@components/ProtectedRoute";
+import MainLayout from "@layouts/MainLayout";
 import { lazy } from "react";
 
 const MainAdminPage = Loadable(lazy(() => import('@modules/admin/MainAdminPage')));
 
 const AdminRoutes = {
   path: '/admin',
-  element: <MainAdminPage />,
-  // children: [
-  //   {
-  //     index: true,
-  //     element: <MainAdminPage />
-  //   },
-  // ],
+  element: <MainLayout />,
+  children: [
+    {
+      index: true,
+      element: <MainAdminPage />
+    },
+  ],
 };
 
 export default AdminRoutes;
