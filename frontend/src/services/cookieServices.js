759b75e759d425388386f2b4e500853f7a7d0a2d
ha-vu <onedvu315itech@gmail.com>
2025-11-11 14:42:47 +0700

    remove log

--- frontend/src/services/cookieServices.js
+++ frontend/src/services/cookieServices.js
@@ -3,7 +3,6 @@ import api from "@configs/api";
 const setCookieAPI = async () => {
   let resOfCookie = await api.get('/users/csrf');
   if (resOfCookie) {
-    console.log('Set cookie successfully!');
     return resOfCookie.data;
   } else {
     return null;
