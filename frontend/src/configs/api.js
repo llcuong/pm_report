d938df6f9f7234dfdc6aa344fbb837cd8f8428a2
ha-vu <onedvu315itech@gmail.com>
2025-11-11 10:34:36 +0700

    remove interceptors and change base url

--- frontend/src/configs/api.js
+++ frontend/src/configs/api.js
@@ -1,27 +1,11 @@
-import getCookie from "@utils/getCookie";
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: '/',                 // Un-direct to backend server, only direct to Vite through proxy configuration in vite.config.js
+  baseURL: 'http://172.18.55.215:17500',                 // Un-direct to backend server, only direct to Vite through proxy configuration in vite.config.js
   headers: {
     'Content-Type': 'application/json',
   },
   withCredentials: true,
 });
-
-// Auto get CSRF Token every requests
-api.interceptors.request.use(
-  config => {
-    let token = getCookie('csrftoken');
-    if (token) {
-      config.headers['X-CSRFToken'] = token;
-    };
-
-    return config;
-  },
-  error => {
-    return Promise.reject(error);
-  }
-);
 
 export default api;
