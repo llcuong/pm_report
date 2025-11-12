233e42c9574918d3c20f27705f0758ee7fecc39d
ha-vu <onedvu315itech@gmail.com>
2025-11-12 09:30:13 +0700

    create constant for all apps

---
+++ frontend/src/constants/AppList.jsx
@@ -0,0 +1,20 @@
+import AdminPanelIcon from "@assets/icons/admin-panel-icon";
+import PinholeIcon from "@assets/icons/pinhole-icon";
+
+export const commonAppList = [
+  {
+    id: 1,
+    name: "Pinhole Report",
+    icon: <PinholeIcon />,
+    link: "pinhole-report",
+  },
+];
+
+export const adminAppList = [
+  {
+    id: 5,
+    name: "Admin Panel",
+    icon: <AdminPanelIcon />,
+    link: "admin",
+  },
+];
