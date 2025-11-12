71530c15f88f96a78bac303d8949af29b7ff0ad6
ha-vu <onedvu315itech@gmail.com>
2025-11-12 09:29:15 +0700

    change border colort

--- frontend/src/components/sidebar/SidebarHeader.jsx
+++ frontend/src/components/sidebar/SidebarHeader.jsx
@@ -14,7 +14,7 @@ export default function SidebarHeader({ isExpanded }) {
           </div>
         )
       }
-      <div className={['h-0.5 bg-gray-400 rounded transition-all ease-linear', isExpanded ? 'w-52' : 'w-10'].join(' ')} />
+      <div className={['h-0.5 bg-gray-600 rounded transition-all ease-linear', isExpanded ? 'w-52' : 'w-10'].join(' ')} />
     </div>
   );
 };
