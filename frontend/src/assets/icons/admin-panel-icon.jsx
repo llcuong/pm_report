f49d1ac1eb912d259dd591cb2e715e65a9d16ac8
ha-vu <onedvu315itech@gmail.com>
2025-11-12 09:22:43 +0700

    add icon

---
+++ frontend/src/assets/icons/admin-panel-icon.jsx
@@ -0,0 +1,21 @@
+const AdminPanelIcon = () => {
+  return (
+    <svg
+      viewBox="0 0 24 24"
+      height="1em"
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <rect x="3" y="3" width="7" height="9" />
+      <rect x="14" y="3" width="7" height="5" />
+      <rect x="14" y="12" width="7" height="9" />
+      <rect x="3" y="16" width="7" height="5" />
+    </svg>
+  );
+};
+
+export default AdminPanelIcon;
---
+++ frontend/src/assets/icons/sign-out-icon.jsx
@@ -0,0 +1,14 @@
+const SignOutIcon = () => {
+  return (
+    <svg className="w-6 h-6" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <title>Sign Out</title>
+      <g stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+        <path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4" />
+        <polyline points="16 17 21 12 16 7" />
+        <line x1="21" y1="12" x2="9" y2="12" />
+      </g>
+    </svg>
+  );
+};
+
+export default SignOutIcon;
