f124676000e35f4d72f7d5ad6deb8d7c7893bc73
ha-vu <onedvu315itech@gmail.com>
2025-11-13 11:28:40 +0700

    add dropdown menu for language options

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,11 +1,22 @@
+import DropdownIcon from "@assets/icons/dropdown-icon";
+import DropdownDataCustom from "./DropdownDataCustom";
+import useLang from "@hooks/useLang";
+import { LANGUAGE_LIST } from "@constants/LanguageList";
+import { useTranslation } from "react-i18next";
+
 export default function Navbar() {
+  const {
+    selectedLang, setSelectedLang,
+  } = useLang();
+
+  const { t } = useTranslation();
+
   return (
     <nav
       className="bg-(--bg-primary) shadow-[0_10px_5px_-3px_var(--navbar-shadow-color),0_4px_6px_-2px_var(--navbar-shadow-color)] sticky top-0 z-50">
       <div className="max-w-8xl mx-auto px-4">
-        <div className="flex items-center h-16">
-
-          <div className="flex items-center flex-1 min-w-0">
+        <div className="flex items-center justify-baseline h-16">
+          <div className="flex items-center w-[15%]">
             <div className="flex items-center shrink-0">
               <div>
                 <a href="/">
@@ -19,12 +30,21 @@ export default function Navbar() {
             </div>
           </div>
 
-          <div className="flex flex-row items-center flex-none mx-4 min-w-0">
+          <div className="flex flex-row items-center justify-end mx-4 w-[79%]">
+            <DropdownDataCustom
+              id='lang'
+              options={LANGUAGE_LIST}
+              value={selectedLang}
+              onChange={setSelectedLang}
+              placeholder="Language"
+              buttonClassName="w-40"
+              menuClassName="w-40"
+            />
           </div>
 
-          <div className="flex items-center justify-end gap-1 flex-1 min-w-0">
+          <div className="flex items-center justify-end gap-1 w-[6%]">
             <span className="text-(--text-secondary) px-3 py-2 text-sm font-medium transition-colors">
-              version 0.1.0
+              {t('navbar.version')} 0.1.0
             </span>
           </div>
         </div>
