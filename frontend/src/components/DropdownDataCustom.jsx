1ef487bc340174b0831810f4c60de4350cfbeb1a
ha-vu <onedvu315itech@gmail.com>
2025-11-13 11:27:46 +0700

    separate to use custom hook for logic and

--- frontend/src/components/DropdownDataCustom.jsx
+++ frontend/src/components/DropdownDataCustom.jsx
@@ -1,5 +1,6 @@
 import DropdownIcon from "@assets/icons/dropdown-icon";
-import { useState, useEffect, useRef } from "react";
+import useDropdownDataCustom from "@hooks/useDropdownDataCustom";
+import { useTranslation } from "react-i18next";
 
 const DropdownDataCustom = ({
   options = [],
@@ -12,95 +13,66 @@ const DropdownDataCustom = ({
   menuMatchButton = false,
   menuAlign = "left",
 }) => {
-  const [isOpen, setIsOpen] = useState(false);
-  const [selectedOption, setSelectedOption] = useState(value || null);
+  const { t } = useTranslation();
 
-  const dropdownRef = useRef(null);
-  const buttonRef = useRef(null);
-  const [btnWidthPx, setBtnWidthPx] = useState(0);
-
-  const toggleDropdown = () => setIsOpen(!isOpen);
-  const closeDropdown = () => setIsOpen(false);
-
-  const handleSelectOption = (option) => {
-    setSelectedOption(option);
-    onChange?.(option);
-    setIsOpen(false);
-  };
-
-  // Handle open & close action of dropdown
-  useEffect(() => {
-    const handleClickOutside = (mouseEvent) => {
-      if (dropdownRef.current && !dropdownRef.current.contains(mouseEvent.target)) {
-        closeDropdown();
-      }
-    };
-    document.addEventListener("click", handleClickOutside);
-    return () => document.removeEventListener("click", handleClickOutside);
-  }, []);
-
-  // Set latest selected option
-  useEffect(() => {
-    setSelectedOption(value || null);
-  }, [value]);
-
-  // Get the width of the select button
-  const measure = () => {
-    const rect = buttonRef.current.getBoundingClientRect();
-    setBtnWidthPx(rect.width);
-  };
-
-  // Resize for each select button
-  useEffect(() => {
-    if (!buttonRef.current) return;
-    measure();
-    if (isOpen) measure();
-    const resizeObserver = new ResizeObserver(measure);
-    resizeObserver.observe(buttonRef.current);
-    return () => resizeObserver.disconnect();
-  }, [isOpen]);
+  const {
+    isOpen,
+    selectedOption,
+    toggleDropdown,
+    handleSelectOption,
+    dropdownRef,
+    buttonRef,
+    btnWidthPx,
+  } = useDropdownDataCustom(value, onChange);
 
   const alignClass = menuAlign === "right" ? "right-0" : "left-0";
 
   return (
-    <div ref={dropdownRef} className='relative'>
+    <div ref={dropdownRef} className="relative">
       <button
         ref={buttonRef}
         type="button"
         onClick={toggleDropdown}
         className={`flex items-center justify-between border-2 bg-white rounded-lg px-4 py-3 text-left
-                    ${isOpen ? `border-[#024A54] shadow-md` : "border-gray-200"}  cursor-pointer transition-all 
-                    duration-200 hover:border-[#024A54] ${buttonClassName}`}
+          ${isOpen ? "border-[#024A54] shadow-md" : "border-gray-200"} cursor-pointer transition-all 
+          duration-200 hover:border-[#024A54] ${buttonClassName}`}
       >
-        <span className={`${isOpen ? `text-[#024A54]` : 'text-gray-900'} text-sm font-medium truncate`}>
-          {selectedOption?.label || placeholder}
+        <span className={`${isOpen ? "text-[#024A54]" : "text-gray-900"} text-sm font-medium truncate`}>
+          {selectedOption?.label || t(placeholder)}
         </span>
-        <div className={`text-[#024A54]`}>
+        <div className="text-[#024A54]">
           <DropdownIcon isOpen={isOpen} />
         </div>
       </button>
 
       {isOpen && (
         <div
-          className={`absolute z-10 mt-1 bg-white border border-gray-200 rounded-lg shadow-lg max-h-60 
-                      overflow-auto ${alignClass} ${menuMatchButton ? "" : menuClassName}`}
+          className={`absolute z-10 mt-1 bg-white border border-gray-200 rounded-lg shadow-lg max-h-60 overflow-auto ${alignClass} ${menuMatchButton ? "" : menuClassName
+            }`}
           style={menuMatchButton ? { width: btnWidthPx ? `${btnWidthPx}px` : undefined } : undefined}
         >
-          {options.map(option => (
+          {options.map((option) => (
             <div
               key={option.value}
               onClick={() => handleSelectOption(option)}
-              className={`cursor-pointer px-4 py-3  transition-colors duration-150 border-b border-gray-100 
-                          last:border-b-0 ${selectedOption?.value === option.value ? `bg-[#024A54]` : "hover:bg-[#aececf]"}`}
+              className={`cursor-pointer px-4 py-3 transition-colors duration-150 border-b border-gray-100 
+                last:border-b-0 ${selectedOption?.value === option.value ? "bg-[#024A54]" : "hover:bg-[#aececf]"}`}
             >
               <div className="flex items-center justify-between">
                 <div className="flex-1">
-                  <div className={`text-sm font-semibold ${selectedOption?.value === option.value ? 'text-white' : 'text-gray-900 '} `}>
+                  <div
+                    className={`text-sm font-semibold ${selectedOption?.value === option.value ? "text-white" : "text-gray-900"
+                      }`}
+                  >
                     {option.label}
                   </div>
                   {option.description && (
-                    <div className={`${selectedOption?.value === option.value ? `bg-[#024A54] text-white` : "hover:bg-[#aececf] text-gray-500"} 
-                                    text-xs mt-1 transition-colors duration-200`}>
+                    <div
+                      className={`text-xs mt-1 transition-colors duration-200 ${selectedOption?.value === option.value
+                        ? "bg-[#024A54] text-white"
+                        : "hover:bg-[#aececf] text-gray-500"
+                        }`}
+                    >
                       {option.description}
                     </div>
                   )}
