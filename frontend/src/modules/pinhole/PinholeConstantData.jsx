5ae7a8ff49a8df13ee8dd7053c1e7b0675fd32d0
ha-vu <onedvu315itech@gmail.com>
2025-11-13 11:34:07 +0700

    move data text to an attribute

--- frontend/src/modules/pinhole/PinholeConstantData.jsx
+++ frontend/src/modules/pinhole/PinholeConstantData.jsx
@@ -4,22 +4,31 @@ export const FACTORY_DATA = [
   { value: "lt", label: "Long Thanh", description: "PVC" },
 ];
 
-export const BRANCH_BY_FACTORY_DATA = {
-  gd: [
-    { value: "PVC", label: "PVC" },
-    { value: "NBR1", label: "NBR1" },
-    { value: "NBR2", label: "NBR2" },
-    { value: "NBR3", label: "NBR3" },
-  ],
-  lk: [
-    { value: "NBR1", label: "NBR1" },
-    { value: "NBR2", label: "NBR2" },
-  ],
-  lt: [
-    { value: "PVC1", label: "PVC1" },
-    { value: "PVC2", label: "PVC2" },
-  ],
-};
+export const BRANCH_BY_FACTORY_DATA = [
+  {
+    id: 'gd',
+    branch: [
+      { value: "PVC", label: "PVC" },
+      { value: "NBR1", label: "NBR1" },
+      { value: "NBR2", label: "NBR2" },
+      { value: "NBR3", label: "NBR3" },
+    ],
+  },
+  {
+    id: 'lk',
+    branch: [
+      { value: "NBR1", label: "NBR1" },
+      { value: "NBR2", label: "NBR2" },
+    ],
+  },
+  {
+    id: 'lt',
+    branch: [
+      { value: "PVC1", label: "PVC1" },
+      { value: "PVC2", label: "PVC2" },
+    ],
+  },
+];
 
 export const HOUR_LIST = [
   ...Array.from({ length: 18 }, (_, i) => String(i + 6).padStart(2, '0')),
