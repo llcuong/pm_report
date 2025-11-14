b6ac6fd2741cbd99f19c5d7659a7a05080cd703d
ha-vu <onedvu315itech@gmail.com>
2025-11-14 16:59:57 +0700

    create component

--- frontend/src/modules/authorizationApp/userProfile/MainUserProfile.jsx
+++ frontend/src/modules/authorizationApp/userProfile/MainUserProfile.jsx
@@ -1,8 +1,22 @@
+import AvatarSection from "./avatar/AvatarSection";
+import ProfileSection from "./profile/ProfileSection";
+import PasswordSection from "./password/PasswordSection";
+
 const MainUserProfile = () => {
   return (
-    <>
-      Profile
-    </>
+    <div className="p-4 h-230 mx-auto flex flex-col md:flex-row gap-10">
+      <div className="md:w-1/5 bg-white shadow-md p-6 rounded-xl">
+        <AvatarSection />
+      </div>
+
+      <div className="md:w-3/5 bg-white shadow-md p-6 rounded-xl">
+        <ProfileSection />
+      </div>
+
+      <div className="md:w-1/5 bg-white shadow-md p-6 rounded-xl">
+        <PasswordSection />
+      </div>
+    </div>
   );
 };
 
