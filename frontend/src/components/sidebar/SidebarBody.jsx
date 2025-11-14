c48bc9675a3ceba5d2f994f65795cc99d0dd1b00
ha-vu <onedvu315itech@gmail.com>
2025-11-14 11:04:37 +0700

    add gap for app list

--- frontend/src/components/sidebar/SidebarBody.jsx
+++ frontend/src/components/sidebar/SidebarBody.jsx
@@ -30,11 +30,11 @@ const SidebarBody = ({ isExpanded }) => {
 
   return (
     <div className="w-full flex-1 min-h-0 overflow-hidden px-1.5 py-1 space-y-1">
-      <div className="h-[50%]">
+      <div className="h-[50%] flex flex-col gap-1">
         <SidebarBodyApp appList={commonAppList} activeAppId={activeAppId} isExpanded={isExpanded} handleOnClickApp={handleOnClickApp} />
       </div>
       <div className={['h-0.5 bg-gray-300 rounded transition-all ease-linear', isExpanded ? 'w-52' : 'w-10'].join(' ')} />
-      <div className="h-[50%]">
+      <div className="h-[50%] flex flex-col gap-1">
         {
           (userName !== defaultUserName || isAdmin !== 'true') ? (
             <></>
