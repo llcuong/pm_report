a13ac22ccffd68eae5bc7af9c7ae9b2c4f038c71
ha-vu <onedvu315itech@gmail.com>
2025-11-17 16:57:53 +0700

    wrap in select app id provider

--- frontend/src/layouts/MainLayout.jsx
+++ frontend/src/layouts/MainLayout.jsx
@@ -1,17 +1,20 @@
 import Navbar from "@components/Navbar";
 import Sidebar from "@components/Sidebar";
+import { SelectedAppIdProvider } from "@contexts/SelectedAppIdContext";
 import { Outlet } from "react-router-dom";
 
 export default function DashboardLayout() {
   return (
     <div className="flex h-screen">
-      <Sidebar />
-      <div className="flex flex-col w-screen">
-        <Navbar />
-        <main className="relative h-screen overflow-y-auto bg-gray-50 p-2">
-          <Outlet />
-        </main>
-      </div>
+      <SelectedAppIdProvider>
+        <Sidebar />
+        <div className="flex flex-col w-screen">
+          <Navbar />
+          <main className="relative h-screen overflow-y-auto bg-gray-50 p-2">
+            <Outlet />
+          </main>
+        </div>
+      </SelectedAppIdProvider>
     </div>
   );
 };
