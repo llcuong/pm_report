5e495cafeabb56adaf2a439969bedb8859edda3e
ha-vu <onedvu315itech@gmail.com>
2025-11-17 17:02:07 +0700

    all attribute of table head

---
+++ frontend/src/modules/authorizationApp/userManagement/dataTable/THead/THeadConstants.js
@@ -0,0 +1,59 @@
+export const THeadAttributeConstants = [
+  {
+    id: 1,
+    label: "ID",
+    attribute: "id",
+    requiredSort: true,
+    width: 10,
+    menu: [
+      { menuId: 1, label: "No sort", value: "normal" },
+      { menuId: 2, label: "Ascending", value: "asc" },
+      { menuId: 3, label: "Descending", value: "desc" },
+    ],
+  },
+  {
+    id: 2,
+    label: "Full Name",
+    attribute: "fullName",
+    requiredSort: false,
+    width: 20,
+  },
+  {
+    id: 3,
+    label: "Role",
+    attribute: "role",
+    requiredSort: false,
+    width: 10,
+  },
+  {
+    id: 4,
+    label: "Email",
+    attribute: "email",
+    requiredSort: false,
+    width: 20,
+  },
+  {
+    id: 5,
+    label: "Department",
+    attribute: "department",
+    requiredSort: true,
+    width: 20,
+    menu: [
+      { menuId: 1, label: "No sort", value: "normal" },
+      { menuId: 2, label: "Ascending", value: "asc" },
+      { menuId: 3, label: "Descending", value: "desc" },
+    ],
+  },
+  {
+    id: 6,
+    label: "Status",
+    attribute: "status",
+    requiredSort: true,
+    width: 5,
+    menu: [
+      { menuId: 1, label: "No sort", value: "normal" },
+      { menuId: 2, label: "Active", value: "active" },
+      { menuId: 3, label: "Inactive", value: "inactive" },
+    ],
+  },
+];
