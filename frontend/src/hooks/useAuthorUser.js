9d4395051f05abaf68d111fda958fe285734970a
ha-vu <onedvu315itech@gmail.com>
2025-11-17 16:57:11 +0700

    add handle when click app

--- frontend/src/hooks/useAuthorUser.js
+++ frontend/src/hooks/useAuthorUser.js
@@ -1,14 +1,19 @@
+import { ACTIVE_APP_ID } from "@components/AppIdWrapper";
 import AuthorUserTaskList from "@constants/AuthorUserTaskList";
 import { useEffect, useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
+import useSelectedAppIdContext from "@contexts/useSelectedAppIdContext";
 
 const useAuthorUser = () => {
   const navigate = useNavigate();
 
   const [isOpen, setIsOpen] = useState(false);
+  const [activeAppId, setActiveAppId] = useState(null);
 
   const dropdownRef = useRef(null);
+
+  const { handleOnClickApp } = useSelectedAppIdContext();
 
   // Click outside to close
   useEffect(() => {
@@ -25,6 +30,7 @@ const useAuthorUser = () => {
   const handleSelectedTask = (taskId) => {
     let navigateLink = AuthorUserTaskList.find(task => task.id === taskId).link;
     if (navigateLink) {
+      handleOnClickApp(taskId);
       navigate(navigateLink);
     } else {
       toast.error('Unknown link to navigate!');
