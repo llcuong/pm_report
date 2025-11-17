2702d360ddae17020bfd1426ac2096b4494efc95
ha-vu <onedvu315itech@gmail.com>
2025-11-17 16:58:24 +0700

    create components for test data

--- frontend/src/modules/authorizationApp/userManagement/MainUserManagement.jsx
+++ frontend/src/modules/authorizationApp/userManagement/MainUserManagement.jsx
@@ -1,8 +1,16 @@
+import DataTable from "./dataTable/DataTable";
+import UserManagementHeader from "./header/UserManangementHeader";
+import Paging from "./paging/Paging";
+
 const MainUserManagement = () => {
   return (
-    <>
-      User Management
-    </>
+    <div className="mt-5 h-220 mx-auto flex md:flex-col gap-10 bg-white shadow-md p-6 rounded-xl">
+      <UserManagementHeader />
+
+      <DataTable />
+
+      <Paging />
+    </div>
   );
 };
 
