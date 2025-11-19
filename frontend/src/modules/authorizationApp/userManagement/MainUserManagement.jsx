1905be0336f5ac1171f3a6c5a8fc50f1700773e1
ha-vu <onedvu315itech@gmail.com>
2025-11-19 16:53:10 +0700

    remove context

--- frontend/src/modules/authorizationApp/userManagement/MainUserManagement.jsx
+++ frontend/src/modules/authorizationApp/userManagement/MainUserManagement.jsx
@@ -1,15 +1,16 @@
-import DataTable from "./dataTable/DataTable";
-import UserManagementHeader from "./header/UserManangementHeader";
-import Paging from "./paging/Paging";
+import { UserManagementProvider } from "./contexts/UserManagementContext";
+import DataTable from "./components/dataTable/DataTable";
+import UserManagementHeader from "./components/header/UserManangementHeader";
+import Paging from "./components/paging/Paging";
 
 const MainUserManagement = () => {
   return (
     <div className="mt-5 h-220 mx-auto flex md:flex-col gap-10 bg-white shadow-md p-6 rounded-xl">
-      <UserManagementHeader />
-
-      <DataTable />
-
-      <Paging />
+      <UserManagementProvider>
+        <UserManagementHeader />
+        <DataTable />
+        <Paging />
+      </UserManagementProvider>
     </div>
   );
 };
