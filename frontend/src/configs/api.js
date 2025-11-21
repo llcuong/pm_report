9000bda3753048f647c9ca8d59f66eff60bfa7ee
llcuong <cuong.le@egvnco.com>
2025-11-21 16:35:00 +0700

    checkpoint 20251121-1634

--- frontend/src/configs/api.js
+++ frontend/src/configs/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: 'http://172.18.55.215:30000',     //baseURL: 'http://10.13.100.101:30000'            // Un-direct to backend server, only direct to Vite through proxy configuration in vite.config.js
+  baseURL: 'http://10.13.100.101:30000',     //baseURL: 'http://10.13.100.101:30000'            // Un-direct to backend server, only direct to Vite through proxy configuration in vite.config.js
   headers: { "Content-Type": "application/json" },
   withCredentials: true,
   xsrfCookieName: "csrftoken",
