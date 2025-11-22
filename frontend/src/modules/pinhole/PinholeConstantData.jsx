f9fba0bcb69d7b6ec280e6a1b584d9be4a5163c7
ha-vu <onedvu315itech@gmail.com>
2025-11-22 16:55:05 +0700

    change to lower case

--- frontend/src/modules/pinhole/PinholeConstantData.jsx
+++ frontend/src/modules/pinhole/PinholeConstantData.jsx
@@ -8,24 +8,24 @@ export const BRANCH_BY_FACTORY_DATA = [
   {
     id: 'gd',
     branch: [
-      { value: "PVC", label: "PVC" },
-      { value: "NBR1", label: "NBR1" },
-      { value: "NBR2", label: "NBR2" },
-      { value: "NBR3", label: "NBR3" },
+      { value: "pvc", label: "PVC" },
+      { value: "nbr1", label: "NBR1" },
+      { value: "nbr2", label: "NBR2" },
+      { value: "nbr3", label: "NBR3" },
     ],
   },
   {
     id: 'lk',
     branch: [
-      { value: "NBR1", label: "NBR1" },
-      { value: "NBR2", label: "NBR2" },
+      { value: "nbr1", label: "NBR1" },
+      { value: "nbr2", label: "NBR2" },
     ],
   },
   {
     id: 'lt',
     branch: [
-      { value: "PVC1", label: "PVC1" },
-      { value: "PVC2", label: "PVC2" },
+      { value: "pvc1", label: "PVC1" },
+      { value: "pvc2", label: "PVC2" },
     ],
   },
 ];
@@ -36,18 +36,18 @@ export const HOUR_LIST = [
 ];
 
 export const HOUR_CLASS_CONFIG = [
-    {
-        id: "class_1",
-        hours: HOUR_LIST.slice(0, 8),   // 06–13
-    },
-    {
-        id: "class_2",
-        hours: HOUR_LIST.slice(8, 16),  // 14–21
-    },
-    {
-        id: "class_3",
-        hours: HOUR_LIST.slice(16, 24), // 22–05
-    },
+  {
+    id: "class_1",
+    hours: HOUR_LIST.slice(0, 8),   // 06–13
+  },
+  {
+    id: "class_2",
+    hours: HOUR_LIST.slice(8, 16),  // 14–21
+  },
+  {
+    id: "class_3",
+    hours: HOUR_LIST.slice(16, 24), // 22–05
+  },
 ];
 
 export const REFRESH_INTERVAL = 300000;
