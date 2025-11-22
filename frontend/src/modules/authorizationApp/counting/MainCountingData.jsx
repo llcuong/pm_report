b3afc90ab34a3feb3d262fe64d20456d4d6da16b
ha-vu <onedvu315itech@gmail.com>
2025-11-22 10:08:44 +0700

    add header

--- frontend/src/modules/authorizationApp/counting/MainCountingData.jsx
+++ frontend/src/modules/authorizationApp/counting/MainCountingData.jsx
@@ -1,8 +1,10 @@
+import CountingHeader from "./components/header/CountingHeader";
+
 const MainCountingData = () => {
   return (
-    <>
-      Counting
-    </>
+    <div className="mt-5 h-220 mx-auto flex md:flex-col gap-10 bg-white shadow-md p-6 rounded-xl">
+      <CountingHeader />
+    </div>
   );
 };
 
