1412ae1e77dd2cad59d99b78ae41798a9685d4c1
ha-vu <onedvu315itech@gmail.com>
2025-11-22 16:42:59 +0700

    wrap app with context

--- frontend/src/modules/authorizationApp/counting/MainCountingData.jsx
+++ frontend/src/modules/authorizationApp/counting/MainCountingData.jsx
@@ -1,9 +1,12 @@
+import { CountingSelectOptionProvider } from "./components/contexts/CountingSelectOptionContext";
 import CountingHeader from "./components/header/CountingHeader";
 
 const MainCountingData = () => {
   return (
     <div className="mt-5 h-220 mx-auto flex md:flex-col gap-10 bg-white shadow-md p-6 rounded-xl">
-      <CountingHeader />
+      <CountingSelectOptionProvider>
+        <CountingHeader />
+      </CountingSelectOptionProvider>
     </div>
   );
 };
