4eb7f84fd5f0bdd09eea4375ed21a05250274148
ha-vu <onedvu315itech@gmail.com>
2025-11-22 16:43:38 +0700

    change to default text instead of get the first element of data for showing

--- frontend/src/modules/authorizationApp/hooks/useFactoryAndBranch.js
+++ frontend/src/modules/authorizationApp/hooks/useFactoryAndBranch.js
@@ -1,29 +1,22 @@
 
 import { BRANCH_BY_FACTORY_DATA, FACTORY_DATA } from "@modules/pinhole/PinholeConstantData";
 import { useEffect, useMemo, useState } from "react";
-import { useTranslation } from "react-i18next";
 import { useSearchParams } from "react-router-dom";
 
 const useFactoryAndBranch = () => {
-  const { t } = useTranslation();
   const [searchParamFactory] = useSearchParams();
 
   const [selectedFactory, setSelectedFactory] = useState(() =>
-    FACTORY_DATA.find(factory => factory.value === searchParamFactory.get('factory')) || FACTORY_DATA[0]
+    FACTORY_DATA.find(factory => factory.value === searchParamFactory.get('factory')) || null
   );
 
   // Change relative branches based on selected factory
   const branchData = useMemo(() => {
     let key = (selectedFactory?.value || "").toLowerCase();
-    return BRANCH_BY_FACTORY_DATA.find(branch => branch.id === key).branch || []
+    return BRANCH_BY_FACTORY_DATA.find(branch => branch.id === key)?.branch || []
   }, [selectedFactory]);
 
-  const [selectedBranch, setSelectedBranch] = useState(branchData[0]);
-
-  // Set up selected branch data again
-  useEffect(() => {
-    setSelectedBranch(branchData[0]);
-  }, [branchData]);
+  const [selectedBranch, setSelectedBranch] = useState(branchData[0] || null);
 
   return {
     selectedFactory, setSelectedFactory,
