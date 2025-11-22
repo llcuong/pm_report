defb03ce3eafc0efb77df141a0e147bee3897943
ha-vu <onedvu315itech@gmail.com>
2025-11-22 16:44:15 +0700

    add dropdown for machine data

--- frontend/src/modules/authorizationApp/ipqc/components/header/IPQCHeader.jsx
+++ frontend/src/modules/authorizationApp/ipqc/components/header/IPQCHeader.jsx
@@ -1,34 +1,30 @@
 import DropdownDataCustom from "@components/DropdownDataCustom";
 import DropdownDateCustom from "@components/DropdownDateCustom";
-import { useTranslation } from "react-i18next";
 import useIPQCSelectOptionContext from "../../contexts/useIPQCSelectOptionContext";
+
 import { FACTORY_DATA } from "@modules/pinhole/PinholeConstantData";
 
-const IPQCHeader = () => {
-  const { t } = useTranslation();
+import { useTranslation } from "react-i18next";
 
+const IPQCHeader = () => {
   const {
     selectedFactory, setSelectedFactory,
     branchData, selectedBranch, setSelectedBranch,
     machineData, selectedMachine, setSelectedMachine,
     selectedDate, setSelectedDate,
-
-    viewData,
-
-    isPendingUI,
-    isFetching,
-    isAPIError,
   } = useIPQCSelectOptionContext();
 
+  const { t } = useTranslation();
+
   return (
-    <div className="flex items-center justify-between mb-2">
-      <h2 className="text-2xl font-bold text-[#1b9eaf]">IPQC Data</h2>
+    <div className="flex items-center justify-between mb-4">
+      <h2 className="text-2xl font-bold text-[#1b9eaf]">{t('sidebar.ipqcData')}</h2>
       <div className="shrink-0 flex gap-3">
         <DropdownDateCustom value={selectedDate} onChange={setSelectedDate} />
 
         <DropdownDataCustom
           options={FACTORY_DATA}
-          placeholder={`${t('outlet.pinholeReport.body.factory')}`}
+          placeholder={`${t('selectOption.selectFactory')}`}
           value={selectedFactory}
           onChange={setSelectedFactory}
           buttonClassName="w-40"
@@ -36,14 +32,23 @@ const IPQCHeader = () => {
         />
 
         <DropdownDataCustom
-          key={selectedFactory?.value}
           options={branchData}
-          placeholder={`${t('outlet.pinholeReport.body.branch')}`}
+          placeholder={`${t('selectOption.selectBranch')}`}
           value={selectedBranch}
           onChange={setSelectedBranch}
-          buttonClassName="w-24"
-          menuClassName="w-24"
+          buttonClassName="w-40"
+          menuClassName="w-40"
           disabled={branchData.length === 0}
+        />
+
+        <DropdownDataCustom
+          options={machineData}
+          placeholder={`${t('selectOption.selectMachine')}`}
+          value={selectedMachine}
+          onChange={setSelectedMachine}
+          buttonClassName="w-50"
+          menuClassName="w-50"
+          disabled={machineData.length === 0}
         />
       </div>
     </div>
